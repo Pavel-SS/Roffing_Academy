8f74b9e8b698da1b544295bb40bb6b64bb300408
Pavel <sosnovskiy.pavel@inbox.ru>
2022-06-23 04:46:11 +0300

    create media

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,31 @@
+// Hamburger
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu__item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+})
+//Cross
+window.addEventListener('DOMContentLoaded', () => {
+    const aside = document.querySelector('.roof__aside'),
+    cross = document.querySelector('.cross');
+
+    cross.addEventListener('click', () => {
+        aside.classList.toggle('roof_active');
+    });
+})
+
 //Modal
 $('[data-modal = call]').on('click', function(){
     $('.overlay, #modal').fadeIn('slow');
